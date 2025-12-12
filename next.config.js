ef29afb0a2374170a11eb20a340f68f550c50271
tbo274714-creator <tbo274714@gmail.com>
2025-12-12 11:18:05 -0800

    Fix GitHub Pages basePath

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repo = "telluride-bush";
+
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
